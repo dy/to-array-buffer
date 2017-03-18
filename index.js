008d79cdf356d601b504b4140ed5df9759711cd9
Dmitry <df.creative@gmail.com>
2017-03-18 13:16:09 -0400

    Freshen up

--- index.js
+++ index.js
@@ -3,23 +3,26 @@
  */
 
 var isAudioBuffer = require('is-audio-buffer');
+var isUri = require('is-data-uri')
+var d2b = require('data-uri-to-buffer')
+var b2ab = require('buffer-to-arraybuffer')
 
 module.exports = function toArrayBuffer (arg, clone) {
-	//zero-len or undefined-like
+	//zero-length or undefined-like
 	if (!arg) return new ArrayBuffer();
 
-	//array buffer already
+	//array buffer
 	if (arg instanceof ArrayBuffer) return clone ? arg.slice() : arg;
 
 	//array buffer view: TypedArray, DataView, Buffer etc
-	//FIXME: as only Buffe obtain the way to provide subArrayBuffer - use that
+	//FIXME: as only Buffer obtains the way to provide subArrayBuffer - use that
 	if (ArrayBuffer.isView(arg)) {
 		if (arg.byteOffset != null) return arg.buffer.slice(arg.byteOffset, arg.byteOffset + arg.byteLength);
 		return clone ? arg.buffer.slice() : arg.buffer;
 	}
 
-	//audio-buffer
-	//FIXME: find a faster way than copying per-channel data
+	//audio-buffer - note that we simply merge data by channels
+	//no encoding or cleverness involved
 	if (isAudioBuffer(arg)) {
 		var data = new Float32Array(arg.length * arg.numberOfChannels);
 
@@ -31,16 +34,32 @@ module.exports = function toArrayBuffer (arg, clone) {
 	}
 
 	//buffer/data nested: NDArray, ImageData etc.
-	//FIXME: NDArrays with custom data type cause butthurt
+	//FIXME: NDArrays with custom data type may be invalid for this procedure
 	if (arg.buffer || arg.data) {
 		var result = toArrayBuffer(arg.buffer || arg.data);
 		return clone ? result.slice() : result;
 	}
 
+	//try to decode data-uri, if any
+	if (typeof arg === 'string') {
+		//valid data uri
+		if (isUri(arg)) {
+			var buf = d2b(arg)
+			var ab = b2ab(buf)
+			return ab
+		}
+		//plain string
+		else {
+			var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
+			var bufView = new Uint16Array(buf);
+			for (var i=0, strLen=arg.length; i<strLen; i++) {
+				bufView[i] = arg.charCodeAt(i);
+			}
+			return buf
+		}
+	}
+
 	//array-like or unknown
-	//hope Uint8Array knows better how to treat the data
-	//some data, like strings, is shitty, in sense it would require encoding etc.
-	//lots of code in dependence.
-	//so it’s up to user to convert string to buffer.
+	//hope Uint8Array knows better how to treat the input
 	return (new Uint8Array(arg.length != null ? arg : [arg])).buffer;
 }
--- readme.md
+++ readme.md
@@ -1,29 +1,37 @@
-Turn a buffer like to an ArrayBuffer.
+Turn a buffer-like to an ArrayBuffer:
+
+* Buffer to ArrayBuffer
+* ArrayBuffer view (TydedArray, FloatArray, DataView etc) to ArrayBuffer
+* [AudioBuffer](http://github.com/audiojs/audio-buffer) to ArrayBuffer
+* ImageData to ArrayBuffer
+* [ndarray](https://github.com/scijs/ndarray) to ArrayBuffer
+* String to ArrayBuffer
+* Array to ArrayBuffer
 
 [![npm install to-array-buffer](https://nodei.co/npm/to-array-buffer.png?mini=true)](https://npmjs.org/package/to-array-buffer/)
 
 ```js
-var toArrayBuffer = require('to-array-buffer');
-var context = require('audio-context');
+var toArrayBuffer = require('to-array-buffer')
+var context = require('audio-context')
 
 //Get array buffer from any object.
-var ab = toArrayBuffer(new Buffer(100));
-var ab = toArrayBuffer(new Float32Array(12));
-var ab = toArrayBuffer(context.createBuffer(2, 200, 3000));
-var ab = toArrayBuffer([1, 2, 3]);
-var ab = toArrayBuffer(imageData);
-var ab = toArrayBuffer(ndarray);
+ab = toArrayBuffer(new Buffer(100))
+ab = toArrayBuffer(new Float32Array(12))
+ab = toArrayBuffer(context.createBuffer(2, 200, 3000))
+ab = toArrayBuffer([1, 2, 3])
+ab = toArrayBuffer(imageData)
+ab = toArrayBuffer(ndarray)
+ab = toArrayBuffer(dataUri)
 
-//Please note that for strings you have to define encoding etc.
-//So create buffer at first.
-var ab = toArrayBuffer(new Buffer('abc'));
+//Please note that for strings you may want to decode/encode etc.
+ab = toArrayBuffer(encode('abc'))
 
-//Please also note that if possible, the result is returned by reference.
-//Pass `clone` second argument to ensure result is cloned.
-var refAb = toArrayBuffer(new ArrayBuffer(12), true);
-var refAb = toArrayBuffer(new Uint8Array([1, 2, 3]), true);
+//Pass `clone` as the second argument to ensure result is cloned.
+refAb = toArrayBuffer(new ArrayBuffer(12), true)
+refAb = toArrayBuffer(new Uint8Array([1, 2, 3]), true)
 ```
 
 ### Related
 
+> [save-file](https://github.com/dfcreative/save-file) — save any input data to file in node/browser.
 > [buffer-to-arraybuffer](https://npmjs.org/package/buffer-to-arraybuffer) — convert surely known Buffer datatype to ArrayBuffer.

023e0eb59e0c11eb14a3e2859121010e679b53bc
dy <df.creative@gmail.com>
2018-11-11 16:33:09 -0500

    Fix tests

--- test.js
+++ test.js
@@ -49,8 +49,8 @@ t.skip('File, Blob', t => {
     if (!global.File || !global.Blob) return t.end()
 
 	var uri = 'data:text/plain;base64,SGVsbG8sIFdvcmxkIQ%3D%3D';
-    let file = new File([uri], 'hw.txt')
-    let blob = new Blob([uri])
+    var file = new File([uri], 'hw.txt')
+    var blob = new Blob([uri])
 
     t.equal(String.fromCharCode.apply(null, new Uint8Array(toAB(file))), 'Hello, World!')
     t.equal(String.fromCharCode.apply(null, new Uint8Array(toAB(blob))), 'Hello, World!')
@@ -98,8 +98,8 @@ t.skip('unicode data-uri', t => {
 
 t('nested arrays', t => {
     t.deepEqual(
-        Array.from(toAB([[0,0,0,0], new Uint8Array([1,1,1,1])]))
         [0,0,0,0,1,1,1,1],
+        new Uint8Array(toAB([[0,0,0,0], new Uint8Array([1,1,1,1])]))
     )
 
     t.end()
